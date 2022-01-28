7a8795141e92a09e66c6ba5ac58041c8e30f40bd
o.marusina.k@gmail.com <o.marusina.k@gmail.com>
2022-01-28 17:48:14 +0300

    Доделать печать

--- OfferMaker/Pdf/PdfControl.xaml.cs
+++ OfferMaker/Pdf/PdfControl.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.IO;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Xps;
 
 namespace OfferMaker.Pdf
 {
@@ -22,12 +26,64 @@ namespace OfferMaker.Pdf
     {
         public PdfControl()
         {
+            //// создаем привязку команды
+            //CommandBinding commandBinding = new CommandBinding();
+            //// устанавливаем команду
+            //commandBinding.Command = ApplicationCommands.Print;
+            //// устанавливаем метод, который будет выполняться при вызове команды
+            //commandBinding.Executed += PrintPdf;
+            //// добавляем привязку к коллекции привязок элемента Button
+            //pdfWithBanner.CommandBindings.Add(commandBinding);
             InitializeComponent();
         }
+        private void PrintPdf(object sender, ExecutedRoutedEventArgs e)
+        {
+            //PrintDialog dialog = new PrintDialog();
+            //dialog.PrintQueue = LocalPrintServer.GetDefaultPrintQueue();
+            //dialog.PrintTicket = dialog.PrintQueue.DefaultPrintTicket;
+            //dialog.PrintTicket.PageOrientation = PageOrientation.Landscape;
 
+            //if (dialog.ShowDialog() == true)
+            //{
+            //    XpsDocumentWriter writer = PrintQueue.CreateXpsDocumentWriter(dialog.PrintQueue);
+            //    writer.WriteAsync(pdfWithBanner.Document as FixedDocument, dialog.PrintTicket);
+            //    return true;
+            //}
+
+            //return false;
+        }
+      
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("молодец");
+            PrintTest();
         }
+       
+        public bool PrintTest() 
+        {
+            PrintDialog dialog = new PrintDialog();
+            dialog.PrintTicket = dialog.PrintQueue.DefaultPrintTicket;
+            dialog.PrintTicket.PageOrientation = PageOrientation.Portrait;
+            dialog.PrintQueue = LocalPrintServer.GetDefaultPrintQueue();
+
+            //var selectedDocumentViewer = ((System.Windows.Controls.ContentControl)tabControl1.SelectedItem).Content;
+            System.Windows.Controls.DocumentViewer dw = (DocumentViewer)((System.Windows.Controls.ContentControl)tabControl1.SelectedItem).Content;
+                //((System.Windows.FrameworkElement)((System.Windows.Controls.ContentControl)tabControl1.SelectedItem).Content);
+
+            if (dialog.ShowDialog() == true)
+            {
+                XpsDocumentWriter writer = PrintQueue.CreateXpsDocumentWriter(dialog.PrintQueue);
+                //writer.WriteAsync(pdfWithBanner.Document as FixedDocument, dialog.PrintTicket);
+                writer.WriteAsync(dw.Document as FixedDocument, dialog.PrintTicket);
+                
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+
+
     }
 }
