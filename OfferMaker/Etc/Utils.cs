8b45b40e53a911fd0d5b24527bcb34507e0fb8f0
o.marusina.k@gmail.com <o.marusina.k@gmail.com>
2022-02-02 17:26:50 +0300

    222

--- OfferMaker/Etc/Utils.cs
+++ OfferMaker/Etc/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,26 +33,75 @@ namespace OfferMaker
             }
             return offer;
         }
+        public Image AddImage(byte[] img)
+        {
+            string path = GetPathToImage(img);
+            if (path != null && path != "")
+            {
+                Image image = new Image(Guid.NewGuid().ToString(), Global.User.Id, path) { IsNew = true };
+                return image;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        /// <summary>
+        /// Получение пути к новому изображению, созданному с использованием редактора
+        /// </summary>
+        /// <returns></returns>
+        private static string GetPathToImage(byte[] img)
+        {
+            byte[] result = img;
 
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string newImagePath = currentDirectory + @"\OldOffer\images\";
+            if (!Directory.Exists(newImagePath))
+            {
+                Directory.CreateDirectory(newImagePath);
+            }
+            string retPath = newImagePath + DateTime.Now.ToShortDateString() + ".png";
+            try
+            {
+                System.IO.File.WriteAllBytes(retPath, result);
+            }
+            catch (Exception ex)
+            {
+                retPath = "";
+            }
+
+            return retPath;
+        }
         static public Offer RestoreOldOffer(Offer offer, OldModelCommercial.MainViewModelContainer mainViewModelContainer, ObservableCollection<User> users, bool isArchive)
         {
             try
             {
-                string phrase = mainViewModelContainer.SelectedUser.Name;
-                string[] words = phrase.Split(' ');
-                //tranlaterVM.Manager.Image = ToImage(mainViewModelContainer.SelectedUser.Foto);
-                Position position = new Position(mainViewModelContainer.SelectedUser.Status);
-                User manager = new User
-                {
-                    FirstName = words[0],
-                    LastName = words[1],
-                    PhoneNumber1 = mainViewModelContainer.SelectedUser.Tel1,
-                    PhoneNumber2 = mainViewModelContainer.SelectedUser.Tel2,
-                    Email = mainViewModelContainer.SelectedUser.Email,
-                    Position = position,
-                    Image =
-                };
-                offer.Manager = manager;
+                //string phrase = mainViewModelContainer.SelectedUser.Name;
+                //string[] words = phrase.Split(' ');
+                ////tranlaterVM.Manager.Image = ToImage(mainViewModelContainer.SelectedUser.Foto);
+                //Position position = new Position(mainViewModelContainer.SelectedUser.Status);
+
+                
+
+                
+                //User manager = new User
+                //{
+                //    FirstName = words[0],
+                //    LastName = words[1],
+                //    PhoneNumber1 = mainViewModelContainer.SelectedUser.Tel1,
+                //    PhoneNumber2 = mainViewModelContainer.SelectedUser.Tel2,
+                //    Email = mainViewModelContainer.SelectedUser.Email,
+                //    Position = position,
+                //};
+
+                //string path = GetPathToImage(mainViewModelContainer.SelectedUser.Foto);
+                //if (path != null && path != "")
+                //{
+                //    Image image = new Image(Guid.NewGuid().ToString(), Global.User.Id, path) { IsNew = true };
+                //    manager.Image = image;
+                //}
+
+                //offer.Manager = manager;
                 offer.OfferCreator = manager;
                 if (isArchive) offer.IsArchive = true;
             }
